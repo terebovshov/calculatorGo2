f2d16d537238054168e149f2c252bc54ee09c315
　　Sergey Terebovshо́v <101879486+terebovshov@users.noreply.github.com>
2024-02-15 00:11:39 +0300

    0.1.0

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
-package calculatorGo2
+// main.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for {
+		input := GetUserInput()
+		fmt.Println("Введенные данные: ", input)
+		if IsExitCommand(input) {
+			HandleExit()
+			return // Прерывание выполнения функции main(), что приведет к завершению программы
+		}
+
+		// Выполнение операции и вывод результата.
+		result := Calculate(input)
+		//	fmt.Println("Результат: ", result)
+		fmt.Println("Результат: \"" + fmt.Sprint(result) + "\"")
+	}
+}
